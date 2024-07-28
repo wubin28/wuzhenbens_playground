bfbcd5c76456f4f42c4b280a918f818e38096c6d
wuzhenben <wzb@wuzhenben.com>
2024-07-28 10:44:40 +0800

    chore:inventory_manager_clojure: create a new project by running command 'lein new app inventory_manager_clojure'

---
+++ inventory_manager_clojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to inventory_manager_clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ inventory_manager_clojure/test/inventory_manager_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns inventory-manager-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [inventory-manager-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
