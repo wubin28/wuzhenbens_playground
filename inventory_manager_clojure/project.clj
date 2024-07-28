d601be76601deb324ac0188fb87af87de6d832f4
wuzhenben <wzb@wuzhenben.com>
2024-07-28 10:50:24 +0800

    chore:inventory_manager_clojure: Add IntelliJ IDEA files to .gitignore and update project.clj
    - Added '.idea/' directory to '.gitignore' to exclude IntelliJ IDEA project files.
    - Updated 'org.clojure/clojure' dependency to version '1.11.3'.
    - Added 'lein-cljfmt' plugin version '0.7.0' for code formatting.
    - Configured 'cljfmt' settings for indentation, removal of consecutive blank lines, and trailing whitespace.

--- inventory_manager_clojure/project.clj
+++ inventory_manager_clojure/project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]]
+  :plugins [[lein-cljfmt "0.7.0"]]
+  :cljfmt {:indents {defn [[:inner 0]]
+                     defmethod [[:inner 0]]
+                     defprotocol [[:inner 0]]}
+           :remove-consecutive-blank-lines? true
+           :remove-trailing-whitespace? true}
   :main ^:skip-aot inventory-manager-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
