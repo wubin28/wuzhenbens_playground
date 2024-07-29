96acefb94ca7e9cb03d19462c1057c6648634514
wuzhenben <wzb@wuzhenben.com>
2024-07-29 21:54:23 +0800

    chore:movie_booking_clojure: create a new project by running command 'lein new app movie_booking_clojure'

---
+++ movie_booking_clojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to movie_booking_clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ movie_booking_clojure/src/movie_booking_clojure/core.clj
@@ -0,0 +1,7 @@
+(ns movie-booking-clojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
